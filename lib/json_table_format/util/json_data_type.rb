307cc5c18bef43c42f25900498d80a9d49544c81
Clay Dunston <claydunston@gmail.com>
2022-01-08 13:36:09 -0600

    Add `JsonDataType` constants

---
+++ lib/json_table_format/util/json_data_type.rb
@@ -0,0 +1,10 @@
+module JsonTableFormat
+  module Util
+    # Determines if & how JSON data can be formatted
+    class JsonDataType
+      ARRAY  = :array
+      OBJECT = :object
+      OTHER  = :other
+    end
+  end
+end

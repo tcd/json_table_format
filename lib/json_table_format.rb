0ddf04ac7fcbb2635b511ab37ed6f7394134bcd3
Clay Dunston <claydunston@gmail.com>
2022-01-08 13:36:24 -0600

    more

--- lib/json_table_format.rb
+++ lib/json_table_format.rb
@@ -1,6 +1,7 @@
-require "json_table_format/version"
+require "json_table_format/classes"
 require "json_table_format/util"
+require "json_table_format/version"
+require "json_table_format/test_data"
 
 # Format JSON in a compact, readable, aligned way.
-module JsonTableFormat
-end
+module JsonTableFormat; end
---
+++ lib/json_table_format/classes.rb
@@ -0,0 +1,4 @@
+module JsonTableFormat
+  # Just like the name says
+  module Classes; end
+end

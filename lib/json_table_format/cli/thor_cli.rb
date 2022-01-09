7e2e606093dedcba72d62f22e9fd6996b7fac012
Clay Dunston <claydunston@gmail.com>
2022-01-09 11:49:35 -0600

    [WIP] maybe use thor for the cli

--- lib/json_table_format/cli/thor_cli.rb
+++ lib/json_table_format/cli/thor_cli.rb
@@ -1,11 +1,59 @@
+require "thor"
+
+require "pry"
+
+# rubocop:disable Style/DocumentationMethod
 module JsonTableFormat
   module CLI
     # Command Line Interface for JsonTableFormat
     class ThorCLI < Thor
+
+      default_task(:default)
+
+      def self.exit_on_failure?()
+        return true
+      end
+
+      # ========================================================================
+      # Default
+      # ========================================================================
+
+      map(["-d", "--default"] => :default)
+      desc("default", "default command")
+      def default()
+        puts("default()")
+        exit(0)
+      end
+
+      # ========================================================================
+      # File
+      # ========================================================================
+
+      map(["-f", "--file"] => :file)
+      desc("-f [FILE]", "Format a file")
+      def file(file_path)
+        puts("file() { file_path: #{file_path} }")
+        binding.pry
+        exit(0)
+      end
+
+      # ========================================================================
+      # STDIN
+      # ========================================================================
+
+      map(["-s", "--stdin"] => :stdin)
+      desc("stdin", "Format JSON content from STDIN")
+      def stdin()
+        puts("stdin() { file_path: #{file_path}")
+        exit(0)
+      end
+
+      # ========================================================================
+      # Version
+      # ========================================================================
+
       desc("version", "Display version")
       map(["-v", "--version"] => :version)
-
-      # @return [void]
       def version()
         puts("json-table-format version #{JsonTableFormat::VERSION}")
         exit(0)
@@ -14,3 +62,4 @@ module JsonTableFormat
     end
   end
 end
+# rubocop:enable Style/DocumentationMethod
